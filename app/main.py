341975155200b0b09e227e6809b4a5e8d70e961c
satyamgondrawar <satyamgondrawar01@gmail.com>
2026-04-19 18:58:08 +0530

    fix CORS h

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 import os
 from datetime import date, timedelta
 from typing import Optional
-
+from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
